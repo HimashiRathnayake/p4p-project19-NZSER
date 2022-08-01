f1c3f3ad3a82901197f7a65dc22ed0fc4961ba6e
Harris <htom380@aucklanduni.ac.nz>
2022-08-01 18:28:16 +1200

    Adds loading of Semaine WAV files
    Co-authored-by: Arathi Biju <arathibiju@users.noreply.github.com>

--- src/utils/semaine_utils.py
+++ src/utils/semaine_utils.py
@@ -1,5 +1,6 @@
 import os
 import glob
+import re
 import pandas as pd
 from librosa import load
 
@@ -10,26 +11,27 @@ One time use only.
 '''
 # Rename folders in semaine that dont have at least 2 text files inside with a "NA"
 def prune_semaine():
+    # Obtain root file paths
     file_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     root = os.path.dirname(os.path.dirname(file_path))
     semaine_path = root + "/data/semaine/sessions"
+    
+    # Iterate through folders for renaming
     for folder in os.listdir(semaine_path):
         if len(glob.glob(semaine_path + "/" + folder + "/*.txt")) < 2:
             os.rename(semaine_path + "/" + folder, semaine_path + "/" + folder + "_NA")
-    return
 
 
 def load_semaine():
+    # Obtain root file paths
     file_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     root = os.path.dirname(os.path.dirname(file_path))
     semaine_path = root + "/data/semaine/sessions"
+    
+    # Iterate through session folders and load wav files for each session
     for folder in os.listdir(semaine_path):
-        if folder.endswith("_NA"):
-            continue
-        else:
-            
-            for file in glob.glob(semaine_path + "/" + folder + "/*.wav"):
-                current_signal = load(file, sr=SAMPLING_RATE)
-                current_signal = [current_signal[0]]
-                signals.append(current_signal)
-    return
+        if not folder.endswith("_NA"):
+            for file in glob.glob(semaine_path + "/" + folder + "/*.wav"): 
+                if (file.find("User HeadMounted") > 0):
+                    signals.append(load(file, sr=SAMPLING_RATE))
+                    
