19bf5c19347f7328c17123af40cd42498df9121b
Harris <htom380@aucklanduni.ac.nz>
2022-08-01 18:42:29 +1200

    Adds Semaine annotation loading and WAV merging
    Co-authored-by: Arathi Biju <arathibiju@users.noreply.github.com>

--- src/utils/semaine_utils.py
+++ src/utils/semaine_utils.py
@@ -1,10 +1,9 @@
 import os
 import glob
-import re
 import pandas as pd
 from librosa import load
 
-signals = []
+semaine_data = []
 SAMPLING_RATE = 16000
 ''' 
 One time use only.
@@ -14,24 +13,37 @@ def prune_semaine():
     # Obtain root file paths
     file_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     root = os.path.dirname(os.path.dirname(file_path))
-    semaine_path = root + "/data/semaine/sessions"
+    sessions_path = root + "/data/semaine/sessions"
     
     # Iterate through folders for renaming
-    for folder in os.listdir(semaine_path):
-        if len(glob.glob(semaine_path + "/" + folder + "/*.txt")) < 2:
-            os.rename(semaine_path + "/" + folder, semaine_path + "/" + folder + "_NA")
+    for folder in os.listdir(sessions_path):
+        if len(glob.glob(sessions_path + "/" + folder + "/*.txt")) < 2:
+            os.rename(sessions_path + "/" + folder, sessions_path + "/" + folder + "_NA")
 
 
 def load_semaine():
     # Obtain root file paths
     file_path = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
     root = os.path.dirname(os.path.dirname(file_path))
-    semaine_path = root + "/data/semaine/sessions"
+    sessions_path = root + "/data/semaine/sessions"
+    annotations_path = root + "/data/semaine/TrainingInput"
     
+    dfs = []
+    signals = []
+
+    # Iterate through TrainingInput folder for CSV annotations
+    for folder in os.listdir(annotations_path):
+        df = pd.read_csv(annotations_path + "/" + folder + '/TU_VA.csv')
+        df.insert(0, 'bundle', folder) # Add filename to dataframe
+        dfs.append(df)
+
     # Iterate through session folders and load wav files for each session
-    for folder in os.listdir(semaine_path):
+    for folder in os.listdir(sessions_path):
         if not folder.endswith("_NA"):
-            for file in glob.glob(semaine_path + "/" + folder + "/*.wav"): 
+            for file in glob.glob(sessions_path + "/" + folder + "/*.wav"): 
                 if (file.find("User HeadMounted") > 0):
                     signals.append(load(file, sr=SAMPLING_RATE))
-                    
+
+    # Merge WAV files and their corresponding annotations
+    for i, df in enumerate(dfs):
+        semaine_data.append([df, signals[i]])
