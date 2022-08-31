0bcde08d41eb8be9c2f9dc22c740a52fa3806a52
Harris Tomy <htom380@aucklanduni.ac.nz>
2022-08-31 17:12:37 +1200

    Implements JL results loading
    Co-authored-by: Arathi Biju <arathibiju@users.noreply.github.com>

--- src/utils/jl.py
+++ src/utils/jl.py
@@ -86,51 +86,92 @@ def test_jl(processor, model):
     m2_aro_ccc = []
     m2_val_ccc = []
 
-    # Iterate through each wav file and calculate the arousal and valence values
-    for i in range(len(f1)):
-        print('Processing wav file ' + str(i + 1) + ' of ' + str(len(f1)))
-        true_values = f1[i][0]
-        signal = f1[i][1]
+    results = []
 
-        # Retrieve true arousal and valence values from dataframe
-        true_aro = true_values['arousal']
-        true_val = true_values['valence']
-
-        # Map true arousal and valence values from [-1, 1] -> [0, 1]
-        true_aro = (true_aro + 1) / 2
-        true_val = (true_val + 1) / 2
-
-        # Load audio signal and split into segments of 0.2 seconds
-        signal = [f1[i][1]]
-        chunks = get_audio_chunks_jl(signal, frame_size = JL_FRAME_SIZE,
-         sampling_rate=SAMPLING_RATE)
+    with open('jl_results_f1.txt', 'w', encoding='utf-8') as f:
+        f.write(f"True arousal,Predicted arousal,True valence,Predicted valence\n")
         
-        if (len(chunks) != len(true_aro)):
-            chunks = chunks[:len(true_aro)]
+        # Iterate through each wav file and calculate the arousal and valence values
+        for i in range(len(f1)):
+            print('Processing wav file ' + str(i + 1) + ' of ' + str(len(f1)))
+            f.write(f"File: {f1[i][0].iloc[0]['bundle']}\n")
+            true_values = f1[i][0]
 
-        sess_pred_aro = []
-        sess_pred_val = []
+            # Retrieve true arousal and valence values from dataframe
+            true_aro = true_values['arousal']
+            true_val = true_values['valence']
 
-        for j, chunk in enumerate(chunks):
-            # Process each chunk and retrieve arousal and valence values
-            chunk = np.array(chunk, dtype=np.float32)
-            results = process_func([[chunk]], SAMPLING_RATE, model=model, processor=processor)
-            sess_pred_aro.append(results[0][0])
-            sess_pred_val.append(results[0][2])
-            print('Processed chunk ' + str(j + 1) + ' of ' + str(len(chunks)))
-            # Print true and predicetd arousal and valence values
-            print('True arousal: ' + str(true_aro.iloc[j]))
-            print('Predicted arousal: ' + str(results[0][0]))
-            print('True valence: ' + str(true_val.iloc[j]))
-            print('Predicted valence: ' + str(results[0][2])+'\n')
+            # Map true arousal and valence values from [-1, 1] -> [0, 1]
+            true_aro = (true_aro + 1) / 2
+            true_val = (true_val + 1) / 2
 
-        # Calculate the CCC value of the predicted arousal and valence values for f1
-        f1_aro_ccc.append(ccc(np.array(true_aro), np.array(sess_pred_aro)))
-        f1_val_ccc.append(ccc(np.array(true_val), np.array(sess_pred_val)))
+            # Load audio signal and split into segments of 0.2 seconds
+            signal = [f1[i][1]]
+            chunks = get_audio_chunks_jl(signal, frame_size = JL_FRAME_SIZE,
+            sampling_rate=SAMPLING_RATE)
+            
+            if (len(chunks) != len(true_aro)):
+                chunks = chunks[:len(true_aro)]
 
-        print(f'Session {i} - CCC arousal: {f1_aro_ccc[i]:.2f}, CCC valence: {f1_val_ccc[i]:.2f}')
+            sess_pred_aro = []
+            sess_pred_val = []
+            
+            for j, chunk in enumerate(chunks):
+                # Process each chunk and retrieve arousal and valence values
+                chunk = np.array(chunk, dtype=np.float32)
+                results = process_func([[chunk]], SAMPLING_RATE, model=model, processor=processor)
+                sess_pred_aro.append(results[0][0])
+                sess_pred_val.append(results[0][2])
+                print('Processed chunk ' + str(j + 1) + ' of ' + str(len(chunks)))
+                # # Print true and predicetd arousal and valence values
+                # print('True arousal: ' + str(true_aro.iloc[j]))
+                # print('Predicted arousal: ' + str(results[0][0]))
+                # print('True valence: ' + str(true_val.iloc[j]))
+                # print('Predicted valence: ' + str(results[0][2])+'\n')
+                # f.write(f"True arousal: {true_aro.iloc[j]}, Predicted arousal: {results[0][0]}, True valence: {true_val.iloc[j]}, Predicted valence: {results[0][2]}\n")
+                f.write(f"{true_aro.iloc[j]},{results[0][0]},{true_val.iloc[j]},{results[0][2]}\n")
+                
+            # Calculate the CCC value of the predicted arousal and valence values for f1
+            f1_aro_ccc.append(ccc(np.array(true_aro), np.array(sess_pred_aro)))
+            f1_val_ccc.append(ccc(np.array(true_val), np.array(sess_pred_val)))
+
+            print(f'Session {i} - CCC arousal: {f1_aro_ccc[i]:.2f}, CCC valence: {f1_val_ccc[i]:.2f}')
     
-    # Iterate through each wav file and calculate the arousal and valence values
+
+
+def load_jl_results():
+    # Load in the results of the JL-corpus test text file
+    jl_results = []
+    file_results = []
+    filename = ''
+
+    with open('jl_results_f1.txt', 'r', encoding='utf-8') as f:
+        for line in f:
+            # Skip the first line
+
+            if line == 'True arousal,Predicted arousal,True valence,Predicted valence\n':
+                continue
+
+            # Store the results of each file into a pandas dataframe
+            if line.startswith('File:'):
+                # if this is the first file then skip
+                if len(file_results) != 0:
+                    df = pd.DataFrame(file_results, columns=['true_aro', 'pred_aro', 'true_val', 'pred_val'])
+                    jl_results.append([filename, df])
+                    filename = line.split('File: ')[1].strip()
+                    file_results = []
+                continue
+            else:
+                # Remove the newline character from the end of the line and append to file_results
+                file_results.append(line.strip().split(','))
+
+            
+    return jl_results
+
+
+
+    
+
 
 
 
