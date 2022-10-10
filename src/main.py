eabfd0598395300148bec4f62aa7d98ad673cc7a
Harris Tomy <htom380@aucklanduni.ac.nz>
2022-10-10 13:58:58 +1300

    Adds cuda check attempt

--- src/main.py
+++ src/main.py
@@ -4,35 +4,30 @@ from model import train_model
 from utils.jl import display_jl_quadrant_chart, display_jl_quadrant_chart_sentence, load_jl_sentence, load_jl_wav_files, move_jl_wav_files, test_jl, load_jl_results, test_jl_sentence, transform_jl_dataset
 from utils.recola import recola_dataset, test_recola
 
-from utils.semaine import test_semaine, transform_semaine_dataset
+from utils.semaine import interpolate_semaine_annotations, load_semaine_datasets, map_semaine_annotations, test_semaine, transform_semaine_dataset, move_semaine_annotation_files, move_semaine_wav_files
 
 # train_dataset, test_dataset = transform_jl_dataset()
 
-# train_dataset, train_dataloader, test_dataset, test_dataloader = recola_dataset()
-trainDataset, testDataset = transform_semaine_dataset()
 # Finetune semaine
+trainDataset, testDataset = load_semaine_datasets()
 train_model(trainDataset, testDataset)
+
+# interpolate_semaine_annotations()
+
 # processor, model = load_model()
 # train_model(train_dataset, test_dataset)
 
 # test_msp(processor, model)
 # test_recola(processor, model)
 # test_semaine(processor, model)
+
 # test_jl(processor, model)
 # test_jl_sentence(processor, model)
 # load_jl_sentence()
 # load_jl_results()
+
 # display_jl_quadrant_chart(10,20)
 # display_jl_quadrant_chart_sentence()
-# move_semaine_wav_files()
-# move_semaine_annotation_files()
-
-# Create lists for storing annotations
-true_val = []
-true_aro = []
-
-pred_val = []
-pred_aro = []
 
 # print(process_func(signal_dummy, sampling_rate))
 #  Arousal    dominance valence
