09b9e047e98c5aad36d5547c963aceae0dda1723
Arathi-Biju <abij996@aucklanduni.ac.nz>
2022-10-10 14:15:58 +1300

    WIP recola write results

--- src/utils/recola.py
+++ src/utils/recola.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import glob
 import numpy as np
@@ -8,7 +9,7 @@ from torch.utils.data import DataLoader
 
 from model import process_func
 from utils.audio import get_audio_chunks_recola
-from utils.calc import ccc, map_arrays_to_w2v
+from utils.calc import ccc, map_arrays_to_w2v, pearson
 
 # Constants
 SAMPLING_RATE = 16000
@@ -81,52 +82,72 @@ def test_recola(processor, model):
     ccc_val = []
 
     # Feed the recola data to the model generate predictions and store them in the lists
+    with open('recola_results.txt', 'w', encoding='utf-8') as f:
+        df = pd.DataFrame(columns=['bundle', 'pearson arousal', 'pearson valence', 'ccc arousal', 'ccc valence'])
+        for i, data in enumerate(recola_data):
+            f.write(f"True arousal,Predicted arousal,True valence,Predicted valence\n")
+            print("Processing file " + str(i) + " out of " + str(len(recola_data)))
+            # Get the true arousal and valence values
+            true_values = data[0]
 
-    for i, data in enumerate(recola_data):
-        print("Processing file " + str(i) + " out of " + str(len(recola_data)))
-        # Get the true arousal and valence values
-        true_values = data[0]
+            # Write the current filename to the results file
+            f.write(f"{true_values['bundle'][0]}\n")
 
-        # Average the true values from the dataframe
-        true_values['aro_average'] = true_values[[ 'A1', 'A2', 'A3', 'A4', 'A5']].mean(axis=1)
-        true_values['val_average'] = true_values[[ 'V1', 'V2', 'V3', 'V4', 'V5']].mean(axis=1)
+            # Average the true values from the dataframe
+            true_values['aro_average'] = true_values[[ 'A1', 'A2', 'A3', 'A4', 'A5']].mean(axis=1)
+            true_values['val_average'] = true_values[[ 'V1', 'V2', 'V3', 'V4', 'V5']].mean(axis=1)
 
-        # Add true values to lists
-        true_aro_mapped = []
-        true_val_mapped = []
-        true_aro_mapped, true_val_mapped = map_arrays_to_w2v(true_values['aro_average'], true_values['val_average'])
-        true_aro.append(true_aro_mapped)
-        true_val.append(true_val_mapped)
+            # Add true values to lists
+            true_aro_mapped = []
+            true_val_mapped = []
+            true_aro_mapped, true_val_mapped = map_arrays_to_w2v(true_values['aro_average'], true_values['val_average'])
+            true_aro.append(true_aro_mapped)
+            true_val.append(true_val_mapped)
 
-        signal = [[data[1][0]]]  # format for process func
+            signal = [[data[1][0]]]  # format for process func
 
-        chunks = get_audio_chunks_recola(
-            signal=signal, frame_size=40, sampling_rate=SAMPLING_RATE)
+            chunks = get_audio_chunks_recola(
+                signal=signal, frame_size=40, sampling_rate=SAMPLING_RATE)
 
-        # Store each files predicted arousal and valence
-        file_pred_aro = []
-        file_pred_val = []
+            # Store each files predicted arousal and valence
+            file_pred_aro = []
+            file_pred_val = []
 
-        for chunk in chunks:
-            chunk = np.array(chunk, dtype=np.float32)
-            results = process_func([[chunk]], SAMPLING_RATE, model=model, processor=processor)
-            file_pred_aro.append(results[0][0])
-            file_pred_val.append(results[0][2])
+            for j, chunk in enumerate(chunks):
+                chunk = np.array(chunk, dtype=np.float32)
+                results = process_func([[chunk]], SAMPLING_RATE, model=model, processor=processor)
+                file_pred_aro.append(results[0][0])
+                file_pred_val.append(results[0][2])
+                # Write the true and predicted arousal and valence values to the results file
+                f.write(f"{true_aro_mapped[j]},{results[0][0]},{true_val_mapped[j]},{results[0][2]}\n")
 
-        min_array_length = min(len(true_aro[i]), len(file_pred_aro))
-        file_pred_aro = file_pred_aro[:min_array_length]
-        file_pred_val = file_pred_val[:min_array_length]
-        true_aro[i] = true_aro[i][:min_array_length]
-        true_val[i] = true_val[i][:min_array_length]
+            min_array_length = min(len(true_aro[i]), len(file_pred_aro))
+            file_pred_aro = file_pred_aro[:min_array_length]
+            file_pred_val = file_pred_val[:min_array_length]
+            true_aro[i] = true_aro[i][:min_array_length]
+            true_val[i] = true_val[i][:min_array_length]
 
-        pred_aro.append(file_pred_aro)
-        pred_val.append(file_pred_val)
+            pred_aro.append(file_pred_aro)
+            pred_val.append(file_pred_val)
 
-        # Calculate the CCC for the predicted arousal and valence values
-        ccc_aro.append(ccc(np.array(true_aro[i]), np.array(file_pred_aro)))
-        ccc_val.append(ccc(np.array(true_val[i]), np.array(file_pred_val)))
-        print(f'Session {i} - CCC arousal: {ccc_aro[i]:.2f}, CCC valence: {ccc_val[i]:.2f}')
+            # Calculate the CCC for the predicted arousal and valence values
+            ccc_aro.append(ccc(np.array(true_aro[i]), np.array(file_pred_aro)))
+            ccc_val.append(ccc(np.array(true_val[i]), np.array(file_pred_val)))
+            pearson_aro = pearson(np.array(true_aro[i]), np.array(file_pred_aro))
+            pearson_val = pearson(np.array(true_val[i]), np.array(file_pred_val))
 
+            # Concat ccc and pearson values to dataframe
+            df = pd.concat([df, pd.DataFrame({'bundle': data[0]['bundle'][0], 'pearson arousal': pearson_aro, 'pearson valence': pearson_val,
+                                              'ccc arousal': ccc_aro[i], 'ccc valence': ccc_val[i]}, index=[0])], ignore_index=True)
+
+            
+            print(f'Session {i} - CCC arousal: {ccc_aro[i]:.2f}, CCC valence: {ccc_val[i]:.2f}')
+            print(f'Session {i} - Pearson arousal: {pearson_aro:.2f}, Pearson valence: {pearson_val:.2f}')
+
+           
+
+        filename = f'recola_results_ - {datetime.now().strftime("%H-%M_%d-%m-%Y")}.csv'
+        df.to_csv(filename, index=False)
     # Calculate the CCC and print it
     print(f'Avg. Semaine Acc. per session - CCC arousal: {np.mean(ccc_aro):.2f}, CCC valence: {np.mean(ccc_val):.2f}')
     return ccc_aro, ccc_val
